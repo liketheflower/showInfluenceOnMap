ded94841e17d52461af2a6d46a473a53c4c4dc91
jimmy shen <jimmy@takemobi.com>
2022-12-13 00:06:06 -0500

    Support plot dummy data of USA on a world map

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ This is an repo to show the researcher's global influence by plot some info on m
 [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg?style=flat-square)](https://github.com/ambv/black)
 
 ## Features  
-to be added
-
+### Add two marker to a country 
+One result can be seen in ![usa](./usa.png)
---
+++ src/vis_on_map.py
@@ -0,0 +1,60 @@
+from typing import List, Tuple
+
+import folium
+import numpy as np
+import pandas as pd
+
+colors = ["#66bd63", "#fdae61"]
+
+
+def vis_on_map(
+    countries: List[Tuple[float, float]],
+    num_researchers_cite_my_work: List[int] = None,
+    num_institutions_cite_my_work: List[int] = None,
+    delta_for_latitude_for_better_vis: float = 1.0,
+    save_fn="index.html",
+    opacity_val=8.0,
+):
+    world_map = folium.Map(tiles="cartodbpositron")
+    N = len(countries)
+    if num_institutions_cite_my_work is None:
+        num_institutions_cite_my_work = [None] * N
+    if num_researchers_cite_my_work is None:
+        num_researchers_cite_my_work = [None] * N
+    for (lat, long), num_researcher, num_inst in zip(
+        countries, num_researchers_cite_my_work, num_institutions_cite_my_work
+    ):
+        if num_researcher is not None:
+            folium.CircleMarker(
+                location=[lat, long],
+                radius=int(num_researcher),
+                color=colors[0],
+                opacity=opacity_val,
+                fill_color=colors[0],
+                fill_opacity=opacity_val,
+                fill=True,
+            ).add_to(world_map)
+        if num_inst is not None:
+            folium.RegularPolygonMarker(
+                location=[lat - delta_for_latitude_for_better_vis, long],
+                radius=int(num_inst),
+                color=colors[1],
+                opacity=opacity_val,
+                fill_color=colors[1],
+                fill_opacity=opacity_val,
+                number_of_sides=6,
+                fill=True,
+            ).add_to(world_map)
+    world_map.save(save_fn)
+
+
+if __name__ == "__main__":
+    # Test based on using USA as a sample
+    countries = [(37.6, -95.665)]
+    num_researchers_cite_my_work = [10]
+    num_institutions_cite_my_work = [20]
+    vis_on_map(
+        countries,
+        num_researchers_cite_my_work=num_researchers_cite_my_work,
+        num_institutions_cite_my_work=num_institutions_cite_my_work,
+    )
